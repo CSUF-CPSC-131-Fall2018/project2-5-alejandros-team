b3f4348a542735169a4f0eb45fb3a6389e542246
tanis62599 <35940295+tanis62599@users.noreply.github.com>
2018-10-05 14:50:58 -0700

    Update Webpage.h

--- Webpage.h
+++ Webpage.h
@@ -1,16 +1,36 @@
 #pragma once
 
+
 #include <string>
 using namespace std;
 
 class Webpage {
 public:
-    Webpage();
-    Webpage(const string& webpageUrl, const time_t& timeVisited);
-    string getUrl();
-    time_t getTime();
+	//def ctor
+	Webpage();
+	Webpage(const string& webpageUrl, const time_t& timeVisited);
+	string getUrl();
+	time_t getTime();
 
 private:
-    string url;
-    time_t time;
+	string url;
+	time_t time;
 };
+//default webpage
+Webpage::Webpage() {
+	url = "blank";
+	time = 0;
+}
+//setter
+Webpage::Webpage(const string& webpageurl, const time_t& timeVisited) {
+	url = webpageurl;
+	time = timeVisited;
+}
+//getter functions
+string Webpage::getUrl() {
+	return url;
+}
+
+time_t Webpage::getTime() {
+	return time;
+}
